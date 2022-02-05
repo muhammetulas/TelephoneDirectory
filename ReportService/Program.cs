8cf11d072ee8658fc529f9abedee2752a56730d7
Muhammet ULAŞ <s.muhammetulas@gmail.com>
2022-02-05 14:34:10 +0300

    RabbitMQ Ayarlamaları yapıldı. Report.API'ye atılan isteklerin RabbitMQ üzerinden yönlendirildiği gözlemlendi.

--- ReportService/Program.cs
+++ ReportService/Program.cs
@@ -1,19 +1,35 @@
 using Microsoft.EntityFrameworkCore;
+using ReportService.API.Messaging.Options;
+using ReportService.API.Messaging.Receiver;
+using ReportService.API.Messaging.Sender;
 using ReportService.Data.Models;
 using ReportService.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var serviceClientSettingsConfig = builder.Configuration.GetSection("RabbitMq");
+var serviceClientSettings = serviceClientSettingsConfig.Get<RabbitMQConfiguration>();
+builder.Services.Configure<RabbitMQConfiguration>(serviceClientSettingsConfig);
+
 // Add services to the container.
 builder.Services.AddDbContext<ReportServiceDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("MyPostgreSQLDbConnection"), b => b.MigrationsAssembly("ReportService.Data")));
-builder.Services.AddSingleton<IReportService, ReportService.Services.Repositories.ReportService>();
+builder.Services.AddScoped<DbContext>(provider => provider.GetService<ReportServiceDbContext>());
+builder.Services.AddScoped<IReportService, ReportService.Services.Repositories.ReportService>();
+builder.Services.AddSingleton<IReportRequestSender, ReportRequestSender>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+if (serviceClientSettings.Enabled)
+{
+    builder.Services.AddHostedService<ReportRequestReceiver>();
+}
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
